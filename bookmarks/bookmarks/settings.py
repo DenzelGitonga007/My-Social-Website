2a193a756e63d41fc800dd1315f1be211af18556
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-30 15:44:16 +0300

    migrate

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Register the app, account
-    'account',
 ]
 
 MIDDLEWARE = [
