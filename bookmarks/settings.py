4ffc28d7cd7f864b13fc32fb5e5f1c4b0f0dad83
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-30 15:30:31 +0300

    project bookmarks

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*i5-=9qyjydbgsb)wt851%n&in*&f0_)!i$7uh!wol0w7r5r7'
+SECRET_KEY = 'django-insecure-9_4k5!#w4(d#kmt+b(k^_*hlq!odqf_5c50lr!fsx%6_9$iugt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
