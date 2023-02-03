bda9a30f9d712e53396191d39ac9d927be79be22
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-03 11:45:05 +0300

    authentication by default django framework

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+# Using the default authentication framework
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'), # 127.0.0.1:8000/login/
+    # previous url
+    # path('login/', views.user_login, name='login'), # 127.0.0.1:8000/login/
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
