07dccb54b2a9fb2d1d359c4d0f2945baaf4e1b40
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-03 11:07:24 +0300

    commenting the urls

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    path('login/', views.user_login, name='login'), # 127.0.0.1:8000/login/
 ]
--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # The path to the login view
     # path('login/', include('account.urls')) # did not work
-    path('', include('account.urls'))
+    path('', include('account.urls')) # includes the urls declared in the urls file of the account app
 ]
