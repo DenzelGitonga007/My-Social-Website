60d244bbbd0d6fca346ff0eb44acf5dd52cd6f24
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-03-04 11:35:41 +0300

    logged in user to change profile-- in views.py

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Import the authentication framework
 from django.contrib.auth import authenticate, login
 # Import the LoginForm class
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserRegistrationForm, ProfileEditForm
 # Using default auth framework for login view
 from django.contrib.auth.decorators import login_required
 
@@ -63,3 +63,25 @@ def register(request):
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
+
+# For the profile
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+        data=request.POST)
+        profile_form = ProfileEditForm(
+        instance=request.user.profile,
+        data=request.POST,
+        files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+        instance=request.user.profile)
+    return render(request,
+    'account/edit.html',
+    {'user_form': user_form,
+    'profile_form': profile_form})
