c3633996ed8477c2c11bae561d224f72d279c779
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-04-20 18:44:59 +0300

    images app

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Sharing images
+    'images.app.ImagesConfig',
 ]
 
 MIDDLEWARE = [
