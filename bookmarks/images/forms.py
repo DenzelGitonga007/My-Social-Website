0978de36abc97a97bee1e1ce62103cb76b3279c4
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-04-27 15:47:38 +0300

    the image can pull from the sites

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,31 +1,30 @@
-# Default save method, and request
+from django import forms
+from .models import Image
 from django.core.files.base import ContentFile
 from django.utils.text import slugify
 import requests
 
-from django import forms
-from .models import Image
 
-# Create an image/share from a site
 class ImageCreateForm(forms.ModelForm):
     class Meta:
-        # Inherit from the Image model
-        model = Image(forms.ModelForm)
-        fields = ['title', 'url', 'description'] # the fields for the user to fill
+        model = Image
+        fields = ['title', 'url', 'description']
         widgets = {
-            'url' : forms.HiddenInput,
+            'url': forms.HiddenInput,
         }
-    
+
     def clean_url(self):
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg', 'png']
         extension = url.rsplit('.', 1)[1].lower()
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not ' \
-            'match valid image extensions.')
+                                        'match valid image extensions.')
         return url
 
-    def save(self, force_insert=False, force_update=False,commit=True):
+    def save(self, force_insert=False,
+                   force_update=False,
+                   commit=True):
         image = super().save(commit=False)
         image_url = self.cleaned_data['url']
         name = slugify(image.title)
@@ -34,7 +33,8 @@ class ImageCreateForm(forms.ModelForm):
         # download image from the given URL
         response = requests.get(image_url)
         image.image.save(image_name,
-        ContentFile(response.content), save=False)
+                         ContentFile(response.content),
+                         save=False)
         if commit:
             image.save()
         return image
