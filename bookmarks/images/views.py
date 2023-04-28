8a7e4b26421962a5181bf743a316694df4517c43
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-04-28 15:38:06 +0300

    view for like and unlike

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,37 +1,9 @@
-# from django.shortcuts import render, redirect
-
-# from django.contrib.auth.decorators import login_required
-# from django.contrib import messages
-# from .forms import ImageCreateForm
-
-# @login_required
-# def image_create(request):
-#     if request.method == 'POST':
-#         # form is sent
-#         form = ImageCreateForm(data=request.POST)
-#         if form.is_valid():
-#             # form data is valid
-#             cd = form.cleaned_data
-#             new_image = form.save(commit=False)
-#             # assign current user to the item
-#             new_image.user = request.user
-#             new_image.save()
-#             messages.success(request,
-#             'Image added successfully')
-#             # redirect to new created item detail view
-#             return redirect(new_image.get_absolute_url())
-#         else:
-#             # build form with data provided by the bookmarklet via GET
-#             form = ImageCreateForm(data=request.GET)
-#         return render(request,
-#         'images/image/create.html',
-#         {'section': 'images',
-#         'form': form})
-
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+
+# Imports for Like and unlike
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
@@ -39,6 +11,8 @@ from django.core.paginator import Paginator, EmptyPage, \
                                   PageNotAnInteger
 from .forms import ImageCreateForm
 from .models import Image
+
+
 
 
 @login_required
@@ -71,3 +45,21 @@ def image_detail(request, id, slug):
     'images/image/detail.html',
     {'section': 'images',
     'image': image})
+
+# Handle the like and unlike
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
