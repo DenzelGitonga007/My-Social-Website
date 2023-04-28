94936512d1e16fa71267614a742c1f03a0bd7dc8
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-04-28 15:39:20 +0300

    url for like and unlike

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -15,8 +15,7 @@ app_name = 'images'
 
 urlpatterns = [
     path('create/', views.image_create, name='create'),
-    path('detail/<int:id>/<slug:slug>/',
-         views.image_detail, name='detail'),
-    # path('like/', views.image_like, name='like'),
+    path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'), # have the image display after saving
+    path('like/', views.image_like, name='like'), # like and unlike
     # path('', views.image_list, name='list'),
 ]
